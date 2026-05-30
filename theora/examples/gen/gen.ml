bfe71094cd1f2008c9d6839b6f8b83239eafbe57
Romain Beauxis <romain.beauxis@gmail.com>
2026-05-30 16:59:49 -0500

    Finish full optinal mode.

---
+++ theora/examples/gen/gen.ml
@@ -0,0 +1,6 @@
+let executable name libraries =
+  Printf.printf "(executable\n (name %s)\n (libraries %s))\n\n" name
+    (String.concat " " libraries)
+
+let () =
+  if Has_theora.available then executable "thtranscode" ["theora"]
